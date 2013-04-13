31d65d34c2dd8a979da3c4d78c3abba13ba8fb1e
Luke Morton <lukemorton.dev@gmail.com>
2013-04-13 09:58:22 +0100

    Whitespace

--- src/Lily/Util/Response.php
+++ src/Lily/Util/Response.php
@@ -2,24 +2,23 @@
 
 namespace Lily\Util;
 
-class Response {
-
-	public static function response(
-		$status,
-		array $headers = array(),
-		$body = '')
-	{
-		return compact('status', 'headers', 'body');
-	}
+class Response
+{
+    public static function response(
+        $status,
+        array $headers = array(),
+        $body = '')
+    {
+        return compact('status', 'headers', 'body');
+    }
   
     public static function redirect($uri, $status = 301)
     {
-    	return static::response($status, array('Location' => $uri));
+        return static::response($status, array('Location' => $uri));
     }
 
     public static function notFound()
     {
-        return static::response(404, array(), 'Not found.');
+        return static::response(404);
     }
-
 }
