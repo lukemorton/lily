be26c31aced96a27767c544b3def7b65be4131cd
Luke Morton <luke@madebymade.co.uk>
2014-02-09 16:29:51 +0000

    Add example of rudimentary acceptance testing

---
+++ test/Lily/Test/Usage/TestingTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Lily\Test\Usage;
+
+use Symfony\Component\DomCrawler\Crawler;
+
+use Lily\Application\MiddlewareApplication;
+use Lily\Application\RoutedApplication;
+
+use Lily\Util\Request;
+use Lily\Util\Response;
+
+class TestingUsageTest extends \PHPUnit_Framework_TestCase
+{
+    private function crawler($html)
+    {
+        return new Crawler($html);
+    }
+
+    private function applicationToTest()
+    {
+        return
+            new MiddlewareApplication(
+                [new RoutedApplication(
+                    [['POST', '/form', file_get_contents(dirname(__FILE__).'/example.html')]])]);
+    }
+
+    public function testFormShouldSuccessfullySubmit()
+    {
+        $application = $this->applicationToTest();
+        $response = $application(Request::post('/form'));
+        $crawler = $this->crawler($response['body']);
+        $this->assertSame(1, $crawler->filter('h1.success')->count());
+    }
+}
