131b6c5d9e7c10fc69a8f8712b759c46e0543ada
Luke Morton <luke@madebymade.co.uk>
2014-02-14 19:32:14 +0000

    Add nested admin application

--- test/Lily/Test/Usage/DescribeComplexApplication.php
+++ test/Lily/Test/Usage/DescribeComplexApplication.php
@@ -14,10 +14,30 @@ class DescribeComplexApplication extends \PHPUnit_Framework_TestCase
 {
     private function application()
     {
+        $admin =
+            new MiddlewareApplication(array(
+                new RoutedApplication(array(
+                    array('GET', '/admin', '<a href="/logout">logout'),
+                    array('GET', '/admin/login', '<form action="post"><button>Login'),
+                )),
+
+                function ($handler) {
+                    return function ($request) use ($handler) {
+                        if ( ! isset($request['cookies']['authed'])) {
+                            return Response::redirect('/admin/login');
+                        }
+
+                        return $handler($request);
+                    };
+                },
+            ));
+
         return
             new MiddlewareApplication(array(
                 new RoutedApplication(array(
                     array('GET', '/', '<a href="/admin">admin'),
+                    array('GET', '/admin', $admin),
+                    array('GET', '/admin/login', $admin),
                 )),
 
                 new MW\Cookie(array('salt' => 'random')),
@@ -29,5 +49,12 @@ class DescribeComplexApplication extends \PHPUnit_Framework_TestCase
         $app = $this->application();
         $response = $app(Request::get('/'));
         $this->assertContains('/admin', $response['body']);
+    }
+
+    public function testAdminRedirectsToLoginIfNotAuthed()
+    {
+        $app = $this->application();
+        $response = $app(Request::get('/admin'));
+        $this->assertSame('/admin/login', $response['headers']['Location']);
     }
 }
