0aae8704eaa1cb5f7f2920d9ef7fe1d8a46d33ce
DrPheltRight <lukemorton.dev@gmail.com>
2014-02-18 10:15:10 +0000

    No need to load example src in main test/bootstrap.php anymore

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -3,6 +3,5 @@
 $loader = require __DIR__.'/../vendor/autoload.php';
 $loader->add('Lily\Test', __DIR__);
 $loader->add('Lily\Mock', __DIR__);
-$loader->addPsr4('Lily\Example\\', __DIR__.'/../example/src');
 
 session_start();
