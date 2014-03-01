c57f3120bd8a5e230d399d64794fa5fa785c2ec2
Luke Morton <lukemorton.dev@gmail.com>
2014-03-01 10:40:02 +0000

    Remove abstraction of RoutedApplication

--- src/Lily/Application/RoutedApplication.php
+++ src/Lily/Application/RoutedApplication.php
@@ -10,9 +10,272 @@
 
 namespace Lily\Application;
 
-use Lily\Application\RoutableApplication;
+use Lily\Util\Response;
 
 /**
  * An application handler that provides routing to other handlers or responses.
  */
-class RoutedApplication extends RoutableApplication {}
+class RoutedApplication
+{
+    // Defines the pattern of a :param
+    const REGEX_PARAM_KEY = ':([a-zA-Z0-9_]++)';
+
+    // What can be part of a :param value
+    const REGEX_PARAM_VALUE = '(?P<$1>[^/.,;?\n]++)';
+
+    // Splat regex
+    const REGEX_SUPER_SPLAT = '\*\*';
+    const REGEX_SUPER_SPLAT_VALUE = '.+';
+    const REGEX_SPLAT = '\*';
+    const REGEX_SPLAT_VALUE = '[^/]+';
+
+    // What must be escaped in the route regex
+    const REGEX_ESCAPE = '[.\\+?[^\\]${}=!|<>]';
+
+    private $routes = array();
+
+    public function __construct($config = NULL)
+    {
+        if (isset($config['routes'])) {
+            $this->routes = $config['routes'];
+        }
+    }
+
+    protected function routes()
+    {
+        return $this->routes;
+    }
+    
+    private function normaliseRoute($route)
+    {
+        if ( ! isset($route[3])) {
+            $route[3] = array();
+        }
+
+        $route[3]['app'] = $this;
+
+        return $route;
+    }
+
+    private function methodMatches($request, $method)
+    {
+        return $method === NULL OR $request['method'] === $method;
+    }
+
+    private function escapeLiterals($uri)
+    {
+        return
+            preg_replace(
+                '#'.static::REGEX_ESCAPE.'#',
+                '\\\\$0',
+                $uri);
+    }
+
+    private function addOptionalPartRegex($uri)
+    {
+        if (strpos($uri, '(') !== FALSE) {
+            // Make optional parts of the URI non-capturing and optional
+            $uri =
+                str_replace(
+                    array('(', ')'),
+                    array('(?:', ')?'),
+                    $uri);
+        }
+
+        return $uri;
+    }
+
+    private function addParamRegex($uri)
+    {
+        return 
+            preg_replace(
+                '#'.static::REGEX_PARAM_KEY.'#',
+                static::REGEX_PARAM_VALUE,
+                $uri);
+    }
+
+    private function addSuperSplatRegex($uri)
+    {
+        return
+            preg_replace(
+                '#'.static::REGEX_SUPER_SPLAT.'#',
+                static::REGEX_SUPER_SPLAT_VALUE,
+                $uri);
+    }
+
+    private function addSplatRegex($uri)
+    {
+        return
+            preg_replace(
+                '#'.static::REGEX_SPLAT.'#',
+                static::REGEX_SPLAT_VALUE,
+                $uri);
+    }
+
+    private function wrapRegex($uri)
+    {
+        return '#^'.$uri.'$#uD';
+    }
+
+    private function uriRegex($uri)
+    {
+        return // blame php 5.3 support for this nesting:
+            $this->wrapRegex(
+                $this->addSplatRegex(
+                    $this->addSuperSplatRegex(
+                        $this->addParamRegex(
+                            $this->addOptionalPartRegex(
+                                $this->escapeLiterals(
+                                    $uri))))));
+    }
+
+    private function removeNumeric(array $mixedArray)
+    {
+        $assocArray = array();
+
+        foreach ($mixedArray as $_k => $_v) {
+            if (is_string($_k)) {
+                $assocArray[$_k] = $_v;
+            }
+        }
+
+        return $assocArray;
+    }
+
+    private function uriMatches($request, $uri)
+    {
+        if ($uri === NULL) {
+            return TRUE;
+
+        // This match might be dangerous if the URL looks like regex... might
+        // that happen?
+        } elseif ($request['uri'] === $uri) {
+            return TRUE;
+        }
+
+        $match =
+            (bool) preg_match(
+                $this->uriRegex($uri),
+                $request['uri'],
+                $matches);
+
+        if (isset($matches[1])) {
+            return $this->removeNumeric($matches);
+        }
+
+        return $match;
+    }
+
+    private function matchedRequest($request, $method, $uri)
+    {
+        if ( ! $this->methodMatches($request, $method)) {
+            return FALSE;
+        }
+
+        $params = $this->uriMatches($request, $uri);
+
+        if ( ! $params) {
+            return FALSE;
+        }
+
+        if (is_array($params)) {
+            $request['route-params'] = $params;
+            $request['params'] = $params + $request['params'];
+        }
+
+        return $request;
+    }
+
+    private function isMap(array $array)
+    {
+        // Keys of the array
+        $keys = array_keys($array);
+
+        // If the array keys of the keys match the keys, then the array must
+        // not be map (e.g. the keys array looked like {0:0, 1:1...}).
+        return array_keys($keys) !== $keys;
+    }
+
+    private function normaliseResponse($response)
+    {
+        if (is_string($response)) {
+            $response = array(
+                'status' => 200,
+                'headers' => array(),
+                'body' => $response,
+            );
+        } elseif ( ! $this->isMap($response)) {
+            list($status, $headers, $body) = $response;
+            $response = compact('status', 'headers', 'body');
+        }
+
+        return $response;
+    }
+
+    private function handlerResponse($handler, $request)
+    {
+        if (is_callable($handler)) {
+            $response = $handler($request);
+        } else {
+            $response = $handler;
+        }
+
+        if ($response === FALSE) {
+            return FALSE;
+        }
+        
+        return $this->normaliseResponse($response);
+
+    }
+
+    private function routeResponse($request, $route)
+    {
+        list($method, $uri, $handler, $additionalRequest) =
+            $this->normaliseRoute($route, $this);
+
+        $request += $additionalRequest;
+
+        $request = $this->matchedRequest($request, $method, $uri);
+
+        if ($request === FALSE) {
+            return FALSE;
+        }
+
+        return $this->handlerResponse($handler, $request);
+    }
+
+    private function matchRoute($request, $routes)
+    {
+        foreach ($routes as $_route) {
+            $response = $this->routeResponse($request, $_route);
+
+            if ($response !== FALSE) {
+                return $response;
+            }
+        }        
+
+        return Response::notFound();
+    }
+
+    protected function parseRoutes($routes)
+    {
+        return $routes;
+    }
+
+    public function __invoke($request)
+    {
+        return $this->matchRoute($request, $this->parseRoutes($this->routes()));
+    }
+
+    public function uri($name, $params = array())
+    {
+        $routes = $this->routes();
+        $uri = $routes[$name][1];
+
+        foreach ($params as $_k => $_v) {
+            $uri = str_replace(":{$_k}", $_v, $uri);
+        }
+
+        return $uri;
+    }
+}
--- test/Lily/Test/Application/RoutedApplication.php
+++ test/Lily/Test/Application/RoutedApplication.php
@@ -10,13 +10,22 @@
 
 namespace Lily\Test\Application;
 
-use Lily\Test\Application\RoutableApplicationTest;
-
 use Lily\Application\RoutedApplication;
+
 use Lily\Mock\RoutedApplicationWithRoutes;
 
-class RoutedApplicationTest extends RoutableApplicationTest
+use Lily\Util\Request;
+use Lily\Util\Response;
+
+class RoutedApplicationTest extends \PHPUnit_Framework_TestCase
 {
+    private function request($method = NULL, $uri = NULL)
+    {
+        return compact('method', 'uri') + array(
+            'params' => array(),
+        );
+    }
+
     protected function applicationWithRoutes()
     {
         return new RoutedApplicationWithRoutes;
@@ -25,5 +34,135 @@ class RoutedApplicationTest extends RoutableApplicationTest
     protected function applicationWithoutRoutes($routes = NULL)
     {
         return new RoutedApplication(compact('routes'));
+    }
+
+    public function routesProvider()
+    {
+        $routes = array(
+            array('GET', '/', 'GET /'),
+            array('GET', '/slug/:a', 'GET /slug/:a'),
+            array('GET', '/slug/:a/:b', 'GET /slug/:a/:b'),
+            array('GET', '/slug/:a/sep/:b', 'GET /slug/:a/sep/:b'),
+            array('GET', '/slug-opt/:a(/sep/:b)', 'GET /slug-opt/:a(/sep/:b)'),
+
+            array('GET', '/user/*', 'GET /user/*'),
+            array('GET', '/admin(/**)', 'GET /admin(/**)'),
+            array('GET', '/resource/*.*', 'GET /resource/*.*'),
+
+            array('HEAD', '/', 'HEAD /'),
+            array('POST', '/', 'POST /'),
+            array('PUT', '/', 'PUT /'),
+            array('DELETE', '/', 'DELETE /'),
+        );
+
+        shuffle($routes);
+
+        $routes[] = array('GET', '/params/:a', function ($request) {
+            return "GET /params/{$request['params']['a']}";
+        });
+
+        $routes[] = array('GET', '/:a', function ($request) {
+            return "GET /{$request['params']['a']}";
+        });
+
+        $app = $this->applicationWithoutRoutes($routes);
+
+        return array(
+            array($app, 'GET', '/', 'GET /'),
+            array($app, 'GET', '/slug/a', 'GET /slug/:a'),
+            array($app, 'GET', '/slug/a/b', 'GET /slug/:a/:b'),
+            array($app, 'GET', '/slug/a/sep/b', 'GET /slug/:a/sep/:b'),
+            array($app, 'GET', '/slug-opt/a', 'GET /slug-opt/:a(/sep/:b)'),
+            array($app, 'GET', '/slug-opt/a/sep/b', 'GET /slug-opt/:a(/sep/:b)'),
+
+            array($app, 'GET', '/user/anything', 'GET /user/*'),
+            array($app, 'GET', '/admin', 'GET /admin(/**)'),
+            array($app, 'GET', '/admin/login', 'GET /admin(/**)'),
+            array($app, 'GET', '/admin/support/tickets', 'GET /admin(/**)'),
+            array($app, 'GET', '/admin/support/tickets/11', 'GET /admin(/**)'),
+            array($app, 'GET', '/resource/users.json', 'GET /resource/*.*'),
+
+            array($app, 'HEAD', '/', 'HEAD /'),
+            array($app, 'POST', '/', 'POST /'),
+            array($app, 'PUT', '/', 'PUT /'),
+            array($app, 'DELETE', '/', 'DELETE /'),
+
+            array($app, 'GET', '/params/else', 'GET /params/else'),
+            array($app, 'GET', '/else', 'GET /else'),
+        );
+    }
+
+    /**
+     * @dataProvider  routesProvider
+     */
+    public function testRouteMatching($handler, $method, $uri, $expected)
+    {
+        $this->assertSame(
+            Response::ok($expected),
+            $handler($this->request($method, $uri)));
+    }
+
+    public function routedApplicationProvider()
+    {
+        return array(
+            array($this->applicationWithRoutes()),
+            array($this->applicationWithoutRoutes(array(
+                array('GET', '/', array(200, array(), 'index')),
+            ))),
+        );
+    }
+
+    /**
+     * @dataProvider routedApplicationProvider
+     */
+    public function testResponseReturnedFromAppHandler($handler)
+    {
+        $this->assertSame(
+            Response::ok('index'),
+            $handler($this->request('GET', '/')));
+    }
+
+    public function testAppAddedToRequest()
+    {
+        $actualApp = FALSE;
+
+        $expectedApp = $this->applicationWithoutRoutes(array(
+            array(NULL, NULL, function ($request) use (& $actualApp) {
+                $actualApp = $request['app'];
+                return '';
+            }),
+        ));
+        $expectedApp($this->request('GET', '/'));
+
+        $this->assertSame($expectedApp, $actualApp);
+    }
+
+    public function testNotFoundRoute()
+    {
+        $handler = $this->applicationWithoutRoutes();
+        $this->assertSame(
+            Response::notFound(),
+            $handler($this->request()));
+    }
+
+    public function testReverseRouting()
+    {
+        $app = $this->applicationWithRoutes();
+        $this->assertSame('/', $app->uri('index'));
+        $this->assertSame(
+            '/slug/test',
+            $app->uri('slug', array('slug' => 'test')));
+    }
+
+    public function testFalseReturnedFromHandlerPassesRequestToNextMatch()
+    {
+        $handler =
+            $this->applicationWithoutRoutes(array(
+                array('GET', '/', FALSE),
+                array('GET', '/', array(200, array(), 'hey')),
+            ));
+
+        $response = $handler(Request::get('/'));
+        $this->assertSame('hey', $response['body']);
     }
 }
