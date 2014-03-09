6363e2edc73d69fa360bff1e408e143fa4e1d3bf
Luke Morton <lukemorton.dev@gmail.com>
2014-03-09 20:28:59 +0000

    Remove old tests for session stores

--- test/Lily/Test/Middleware/Session/NativeSessionStoreTest.php
+++ test/Lily/Test/Middleware/Session/NativeSessionStoreTest.php
@@ -37,42 +37,4 @@ class NativeSessionStoreTest extends SessionStoreTest
             'session' => array($key => $value),
         );
     }
-
-    public function testWriteToSession()
-    {
-        $expectedSession = array('a' => 1);
-        
-        $mw = new Session;
-
-        $wrappedHandler =
-            $mw(
-                function () use ($expectedSession) {
-                    $session = $expectedSession;
-                    return Res::ok() + compact('session');
-                });
-
-        $wrappedHandler(Req::get('/'));
-
-        $this->assertSame($expectedSession, $_SESSION);
-    }
-
-    public function testReadSession()
-    {
-        $_SESSION = array('b' => 2);
-
-        $actualSession = NULL;
-
-        $mw = new Session;
-
-        $wrappedHandler =
-            $mw(
-                function ($request) use (& $actualSession) {
-                    $actualSession = $request['session'];
-                    return Res::ok();
-                });
-
-        $wrappedHandler(Req::get('/'));
-
-        $this->assertSame($_SESSION, $actualSession);
-    }
 }
